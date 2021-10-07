4d610e2649852d898bc3801e546ead12e0f8772c
jjw7795 <jjw7795@naver.com>
2021-10-07 21:07:54 +0900

    ddzdfdfd

--- demo/demo/src/main/java/com/eduNews/demo/controller/HomeController.java
+++ demo/demo/src/main/java/com/eduNews/demo/controller/HomeController.java
@@ -1,2 +1,13 @@
-package com.eduNews.demo.controller;public class HomeController {
+package com.eduNews.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/h")
+    public String a(){
+        return "hello";
+    }
 }
